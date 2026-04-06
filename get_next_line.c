6ea4d86d88dd4c0f8e92d26796dfbf4711f17dac
Ali Khan <alkhan@f0r6s5.codam.nl>
2026-04-06 15:54:35 +0200

    getnextline new functions

--- get_next_line.c
+++ get_next_line.c
@@ -11,60 +11,83 @@
 /* ************************************************************************** */
 
 #include "get_next_line.h"
-#include <string.h>
-#define BUFFER_SIZE 1024
 
-/* 
-char	*return_str(void)
+char	*ft_findnewline(int fd, char *buffer)
 {
-	static char *str;
-} */
+	char	*line;
+	int		i;
+	int		j;
+	char	*temp_buffer;
+
+	i = 0;
+	j = 0;
+	while (buffer[i])
+	{
+		if (buffer[i] == '\n')
+		{
+			while (j <= i)
+				line[j] = buffer[j];
+		}
+		else
+		{
+			buffer = malloc(BUFFER_SIZE + 1);
+			if (!buffer)
+				return (NULL);
+			buffer = buffer + (read(fd, buffer, BUFFER_SIZE));
+		}
+		i++;
+	}
+	return (line);
+}
 char	*get_next_line(int fd)
 {
-	char	*buffer;
-	ssize_t	bytes;
-	char	*ptr;
-	int		len;
-	char	*line;
+	static char	*buffer;
+	ssize_t		bytes;
+	char		*ptr;
+	int			len;
+	char		*line;
 
 	buffer = malloc(BUFFER_SIZE + 1);
-	if(!buffer)
-		return(NULL);
+	if (!buffer)
+		return (NULL);
 	bytes = read(fd, buffer, BUFFER_SIZE);
 	if (bytes <= 0)
 		return (NULL);
-	buffer[bytes] = '\0';
-	ptr = strchr(buffer, '\n');
+	if (bytes < BUFFER_SIZE)
+		// laatste gedeelte van tekst bereikt
+		buffer[bytes] = '\0';
+	if (ft_findnewline(fd, buffer) == 0)
+		return ("no new line found, but run ");
+	ptr = ft_strchr(buffer, '\n');
 	if (!ptr)
 		return (NULL);
 	len = (ptr - buffer) + 1;
 	line = malloc(len + 1);
 	if (!line)
 		return (NULL);
-	ft_memcpy(line, buffer, len);
 	line[len] = '\0';
-	return (line);
+	return (line = ft_memcpy(line, buffer, len));
 }
 
 int	main(void)
 {
-	int		fd;
-	char	*line;
+	int fd;
+	char *line;
 
-	fd = open("akjv.txt", O_RDONLY);
+	fd = open("testbestand2.txt", O_RDONLY);
 	line = get_next_line(fd);
-/* 		while(line)
-		{
-			line = get_next_line(fd);
-			if(!line)
-				break ;
-			printf("%s", line);
-			free(line);
-		} */
-	if (line)
+	while (line)
 	{
+		line = get_next_line(fd);
+		if (!line)
+			break ;
 		printf("%s", line);
 		free(line);
 	}
+	/* if (line)
+	{
+		printf("%s", line);
+		free(line);
+	} */
 	close(fd);
 }
--- get_next_line.h
+++ get_next_line.h
@@ -12,6 +12,7 @@
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
+# define BUFFER_SIZE 1024
 # include <fcntl.h>
 # include <limits.h>
 # include <stdio.h>
